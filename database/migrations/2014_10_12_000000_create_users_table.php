91e29cd5ccd7c843ea15abeb81b18a089cbea057
kadesory92 <sorybana92@gmail.com>
2023-02-27 16:42:49 +0100

    Commit Project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender')->default('M')->comment('M for Male, F Female');;
+            $table->string('tel');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('photo');
+            $table->string('filePassport');
+            $table->rememberToken();
+            $table->string('utype')->default('USR')->comment('ADM for Admin, PS for Personnal and USR for User');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
